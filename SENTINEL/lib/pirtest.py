720a2929db83aa9f3ebb5de96ef81e9ec56a5042
Francis <francis@nix.local>
2021-01-15 14:48:11 +0800

    addeed project code

---
+++ SENTINEL/lib/pirtest.py
@@ -0,0 +1,33 @@
+"""
+Package for interfacing with Raspberry PI PIR motion sensor.
+"""
+from gpiozero import MotionSensor
+import RPi.GPIO as GPIO
+
+
+class MotionDetector:  # pylint: disable=too-few-public-methods
+    """
+    Class to interfaces with Raspberry PI PIR motion sensor module
+    """
+
+    def __init__(self):
+        self.pir = MotionSensor(4)
+
+    def movement_detected(self):
+        """
+        Check if movement detected.
+        :return: boolean
+        """
+        return bool(self.pir.motion_detected)
+
+
+pir = MotionDetector()
+
+try:
+    while True:
+        if bot.is_listen and pir.movement_detected():
+            print("hi")
+        else:
+            time.sleep(1)
+except KeyboardInterrupt:
+    del camera
